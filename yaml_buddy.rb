0badeea61eaccb424c755d6ceff42e00587a8632
kenlyx <kenlyx1124@g.ncu.edu.tw>
2017-09-30 18:28:29 +0800

    Update yaml_buddy.rb

--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,16 @@
 # Module that can be included (mixin) to take and output Yaml data
+# Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  # takes a yaml string and converts it into a data structure in @data.
+  def take_yaml(yaml)
+    require 'yaml'
+    @data = YAML.load(yaml)
+  end
+
+  # converts any data in @data into a yaml string.
+  def to_yaml
+    require 'yaml'
+    yaml_string = @data.to_yaml
+    return yaml_string
+  end
+end
